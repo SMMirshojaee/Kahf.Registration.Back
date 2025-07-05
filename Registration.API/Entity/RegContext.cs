c0735ba26673249237fffbe379424ee05c3fdceb
SeyyedMohammadMirshojaee <sm.mirshojaee@yahoo.com>
2025-07-05 16:51:50 +0330

    scaffold

--- Registration.API/Entity/RegContext.cs
+++ Registration.API/Entity/RegContext.cs
@@ -7,10 +7,6 @@ namespace Registration.API.Entity;
 
 public partial class RegContext : DbContext
 {
-    public RegContext()
-    {
-    }
-
     public RegContext(DbContextOptions<RegContext> options)
         : base(options)
     {
@@ -33,10 +29,6 @@ public partial class RegContext : DbContext
     public virtual DbSet<Regs> Regs { get; set; }
 
     public virtual DbSet<Steps> Steps { get; set; }
-
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=.;Database=Kahf.Registration;User Id=sa;Password=master;TrustServerCertificate=True");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
---
+++ Registration.API/Entity/RegContext.partial.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Registration.API.Entity;
+
+public partial class RegContext
+{
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<ApplicantFormValues>().HasQueryFilter(e => !e.Deleted);
+    }
+}
